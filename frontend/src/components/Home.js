9f8120ee4bc3227cde98e18ed4f08a5744b7302f
jramosp28 <javierrampob@campus.monlau.com>
2023-05-05 19:52:03 +0200

    first commit

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="container1">
+      <h1>Bienvenido a mis Juegos Online </h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/game1">Piedra Papel Tijera</Link>
+          </li>
+          <li>
+            <Link to="/game2">Tamagotchi</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Home;
