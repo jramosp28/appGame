99b3c9f3c67dc24c2aee9fb3c825d8787a1dfaff
jramosp28 <javierrampob@campus.monlau.com>
2023-05-17 19:31:55 +0200

    actualizado bien

--- frontend/src/components/Game2.jsx
+++ frontend/src/components/Game2.jsx
@@ -1,125 +1,293 @@
-import React, { useState, useEffect } from 'react';
-import './Game2.css';
-
-function ProgressBar({ value, maxValue }) {
-  const percentage = (value / maxValue) * 100;
-  let color = 'green';
-  if (percentage < 20) {
-    color = 'red';
-  } else if (percentage < 80) {
-    color = 'yellow';
-  }
-  const styles = {
-    backgroundColor: color,
-    width: `${percentage}%`,
-    height: '40px',
-    marginBottom: '10px',
-    position: 'relative',
-  };
-  const labelStyles = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    color: 'black',
-    fontSize: '20px',
-    textShadow: '1px 1px 1px black',
-  };
-  return (
-    <div style={styles}>
-      <div style={labelStyles}>{`${percentage.toFixed(1)}%`}</div>
-    </div>
-  );
-}
-
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./Game2.css";
+import happy from "../assets/happy.jpg";
+import normal from "../assets/normal.jpg";
+import triste from "../assets/triste.jpg";
+import dead from "../assets/dead.webp";
+import playImg from "../assets/play.png";
+import sleepImg from "../assets/sleep.png";
+import feedImg from "../assets/feed.png";
 function Tamagotchi() {
-  const [hunger, setHunger] = useState(0);
-  const [happiness, setHappiness] = useState(100);
+  const [name, setName] = useState("Tamagotchi");
+  const [nameInput, setNameInput] = useState("");
+  const [nameEditable, setNameEditable] = useState(true);
+  const [age, setAge] = useState(0);
+  const [hunger, setHunger] = useState(100);
   const [health, setHealth] = useState(100);
+  const [happiness, setHappiness] = useState(100);
+  const [energy, setEnergy] = useState(100);
+  const [isDead, setIsDead] = useState(false);
+  const [isAlive, setIsAlive] = useState(true);
+  const [intervalId, setIntervalId] = useState(null);
+  const [date, setDate] = useState("");
+  const [canInteract, setCanInteract] = useState(false);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setHunger((prevHunger) => prevHunger + 1);
-      setHappiness((prevHappiness) => prevHappiness - 1);
-      setHealth((prevHealth) => prevHealth - 1);
+    const intervalId = setInterval(() => {
+      if (!isDead) {
+        console.log(age);
+        
+        setAge((age) => age + 1);
+        setHealth((health) => Math.max(0, health - 5));
+        setHappiness((happiness) => Math.max(0, happiness - 5));
+        setHunger((hunger) => Math.max(0, hunger - 5));
+        setEnergy((energy) => Math.max(0, energy - 5));
+        
+      }
     }, 3000);
-    return () => clearInterval(interval);
-  }, []);
+    return () => clearInterval(intervalId);
+  }, [isDead]);
+  useEffect(()=>{
+    if (age > 0 && date == "") {
+      setDate(new Date().toLocaleString());
+      console.log(date);
+    }
+    if (age >= 1) {
+      setCanInteract(true);
+    }
+  },[age]);
 
   useEffect(() => {
-    const gameData = {
-      hunger,
-      happiness,
-      health,
-    };
+    if (health === 0 || happiness === 0 || hunger === 0 || energy === 0) {
+      console.log("dead");
+      setNameEditable(false);
+      handleDeath();
+      console.log(isDead);
+    }
+  }, [health, happiness, hunger]);
 
-    fetch("http://localhost:3000/game2/", {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(gameData),
+  const handleChangeName = (event) => {
+    setNameInput(event.target.value);
+  };
+
+  const handleSubmitName = (event) => {
+    event.preventDefault();
+    if (nameInput) {
+      setName(nameInput);
+      setNameInput("");
+      setNameEditable(false);
+    }
+  };
+
+  const handleEditName = () => {
+    setNameEditable(true);
+  };
+
+  const handleResetName = () => {
+    setName("Tamagotchi");
+    setNameEditable(true);
+  };
+  const handleRestart = () => {
+    setIsDead(false);
+    setHunger(100);
+    setHappiness(100);
+    setEnergy(100);
+    setHealth(100);
+    setAge(0);
+    setDate("");
+  };
+  const handleDeath = () => {
+    clearInterval(intervalId);
+    setIsDead(true);
+     // Datos a enviar a la API
+  const tamagotchiData = {
+    fechaNacimiento: date,
+    nombre: name,
+    nivelHambre: hunger,
+    nivelEnergia: energy,
+    nivelFelicidad: happiness,
+  };
+
+  // Realizar la solicitud POST a la API
+  fetch('http://localhost:3000/game2', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(tamagotchiData),
+  })
+    .then((response) => {
+      // Manejar la respuesta de la API
+      if (response.ok) {
+        console.log('Datos del Tamagotchi enviados correctamente.');
+      } else {
+        console.log('Error al enviar los datos del Tamagotchi.');
+      }
     })
-      .then((response) => {
-        if (response.ok) {
-          console.log('Datos del juego enviados correctamente.');
-        } else {
-          console.log('Error al enviar los datos del juego.');
-        }
-      })
-      .catch((error) => {
-        console.log('Error en la solicitud POST:', error);
-      });
-  }, [hunger, happiness, health]);
+    .catch((error) => {
+      console.log('Error en la solicitud POST:', error);
+    });
+  };
 
   const feed = () => {
-    setHunger((prevHunger) => Math.max(0, prevHunger - 20));
-    setHappiness((prevHappiness) => Math.min(100, prevHappiness + 5));
-    setHealth((prevHealth) => Math.min(100, prevHealth + 5));
+    if (canInteract) {
+    setHealth((health) => Math.min(100, health + 5));
+    setHunger((hunger) => Math.min(100, hunger + 20));
+    setHappiness((happiness) => Math.min(100, happiness + 5));
+    setEnergy((energy) => Math.min(100, energy + 5));
+    }
   };
-
   const play = () => {
-    setHunger((prevHunger) => Math.min(100, prevHunger + 5));
-    setHappiness((prevHappiness) => Math.min(100, prevHappiness + 20));
-    setHealth((prevHealth) => Math.max(0, prevHealth - 5));
+    if (canInteract) {
+    setHappiness((happiness) => Math.min(100, happiness + 20));
+    setHealth((health) => Math.min(100, health - 5));
+    setHunger((hunger) => Math.min(100, hunger - 5));
+    setEnergy((energy) => Math.max(0, energy - 20));
+    }
   };
-
   const sleep = () => {
-    setHunger((prevHunger) => Math.max(0, prevHunger - 5));
-    setHappiness((prevHappiness) => Math.max(0, prevHappiness - 5));
-    setHealth((prevHealth) => Math.min(100, prevHealth + 10));
+    if (canInteract) {
+    setHappiness((happiness) => Math.min(100, happiness - 5));
+    setHealth((health) => Math.min(100, health + 10));
+    setHunger((hunger) => Math.min(100, hunger - 5));
+    setEnergy((energy) => Math.min(100, energy + 10));
+    }
   };
-
+  const getBarColor = (value) => {
+    if (value > 80) {
+      return "green";
+    } else if (value > 20) {
+      return "yellow";
+    } else {
+      return "red";
+    }
+  };
+  const getFaceStatus = (value) => {
+    if (value > 80) {
+      return happy;
+    } else if (value > 20) {
+      return normal;
+    } else if (value > 0) {
+      return triste;
+    } else {
+      return dead;
+    }
+  };
+  const getLowestStatusValue = () => {
+    return Math.min(health, happiness, hunger, energy);
+  };
+  
   return (
-    <div className="tamagotchi-container">
-      <h1>Tamagotchi</h1>
-      <div className="tamagotchi-stats">
-        <div className="stat hunger">
-          <p>Hunger</p>
-          <ProgressBar value={hunger} maxValue={100} />
+    <div className="main">
+      <div className="screen">
+        <h1 className="text-3xl font-bold mb-4">
+          {nameEditable ? (
+            <>
+              <form
+                onSubmit={handleSubmitName}
+                className="flex justify-between items-center"
+              >
+                <input
+                  type="text"
+                  value={nameInput}
+                  onChange={handleChangeName}
+                  className="border border-gray-300 px-4 py-2 w-4/5 mr-2"
+                />
+                <button
+                  type="submit"
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >
+                  OK
+                </button>
+              </form>
+            </>
+          ) : (
+            <>
+              <div className="border border-gray-300 px-4 py-2">{name}</div>
+              <button
+                onClick={handleEditName}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4"
+              >
+                Edit
+              </button>
+            </>
+          )}
+        </h1>
+        <img className="state" src={getFaceStatus(getLowestStatusValue())}></img>
+        <div className="stats">
+          <p>Age: {age}</p>
+          <p className="mt-4">Fecha de nacimiento: {date}</p>
+
+          <p>Health:</p>
+          <div
+            className="progress-bar"
+            style={{
+              width: `${health}%`,
+              backgroundColor: getBarColor(health),
+            }}
+          >
+            {health}
+          </div>
+
+          <p>Happiness:</p>
+          <div
+            className="progress-bar"
+            style={{
+              width: `${happiness}%`,
+              backgroundColor: getBarColor(happiness),
+            }}
+          >
+            {happiness}
+          </div>
+
+          <p>Hunger:</p>
+          <div
+            className="progress-bar"
+            style={{
+              width: `${hunger}%`,
+              backgroundColor: getBarColor(hunger),
+            }}
+          >
+            {hunger}
+          </div>
+
+          <p>Energy:</p>
+          <div
+            className="progress-bar"
+            style={{
+              width: `${energy}%`,
+              backgroundColor: getBarColor(energy),
+            }}
+          >
+            {energy}
+          </div>
         </div>
-        <div className="stat happiness">
-          <p>Happiness</p>
-          <ProgressBar value={happiness} maxValue={100} />
-        </div>
-        <div className="stat health">
-          <p>Health</p>
-          <ProgressBar value={health} maxValue={100} />
-        </div>
+        {health === 0 || happiness === 0 || hunger === 0 || energy === 0 ? (
+          <div>
+            <p>Your Tamagotchi has died.</p>
+          </div>
+        ) : (
+          
+          <div>
+            <div className="butts">
+              <button className="btt" onClick={feed}>
+                <img className="buttImg" src={feedImg}></img>
+              </button>
+              <button className="btt" onClick={play}>
+                <img className="buttImg" src={playImg}></img>
+              </button>
+              <button className="btt" onClick={sleep}>
+                <img className="buttImg" src={sleepImg}></img>
+              </button>
+            </div>
+          </div>
+        )}
+        {isDead && (
+          <button
+            onClick={handleRestart}
+            className="mt-7 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded "
+          >
+            Restart
+          </button>
+        )}
       </div>
-      <div className="tamagotchi-actions">
-        <button className="action feed" onClick={feed}>
-          Feed
+      <Link to="/">
+        <button className="mt-7 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ">
+          Volver
         </button>
-        <button className="action play" onClick={play}>
-          Play
-        </button>
-        <button className="action sleep" onClick={sleep}>
-          Sleep
-        </button>
-      </div>
+      </Link>
     </div>
   );
 }
-
 export default Tamagotchi;
--- frontend/src/components/Records.jsx
+++ frontend/src/components/Records.jsx
@@ -1,66 +1,112 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./Record.css"; 
 
-const Records = () => {
-    const [gameResults, setGameResults] = useState([]);
-    const [selectedGame, setSelectedGame] = useState('');
+function Records() {
+  const [selectedGame, setSelectedGame] = useState(null);
+  const [partidas, setPartidas] = useState([]);
+  const [mascotas, setMascotas] = useState([]);
+  const fetchPartidas = async () => {
+    const response = await fetch('http://localhost:3000/game1');
+    const jsonData = await response.json();
+    setPartidas(jsonData);
+  };
+  const fetchMascotas = async () => {
+    const response = await fetch('http://localhost:3000/game2');
+    const jsonData = await response.json();
+    setMascotas(jsonData);
+  };
+  useEffect(() => {
+    fetchMascotas();
+  }, []);
+  useEffect(() => {
+    fetchPartidas();
+  }, []);
+    
+  const handleGameClick = (game) => {
+    setSelectedGame(game);
+  };
 
-    useEffect(() => {
-        // Llama a la API para obtener los resultados del juego seleccionado
-        const fetchGameResults = async () => {
-            try {
-                const response = await axios.get(`/${selectedGame}`); // Corregir la ruta a `/api/records/${selectedGame}`
-                setGameResults(response.data); // No es necesario acceder a `response.data.results`
-            } catch (error) {
-                console.error('Error al obtener los resultados del juego:', error);
-            }
-        };
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-red-800">
+      <div className="rounded-lg p-8 bg-white">
+        <div className="max-w-md mx-auto">
+          <h1 className="text-3xl mb-4 text-center font-bold">Registros</h1>
+          <div className="flex justify-between">
+            <button
+              className={`btnRec text-xl font-bold ${
+                selectedGame === "tamagotchi" ? "underline" : ""
+              }`}
+              onClick={() => handleGameClick("tamagotchi")}
+            >
+              Tamagotchi
+            </button>
+            <button
+              className={`btnRec text-xl font-bold ${
+                selectedGame === "rpsls" ? "underline" : ""
+              }`}
+              onClick={() => handleGameClick("rpsls")}
+            >
+              RPSLS
+            </button>
+          </div>
+          {selectedGame === "tamagotchi" && (
+            <div>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Nombre</th>
+                    <th>Nivel de hambre</th>
+                    <th>Nivel de energía</th>
+                    <th>Nivel de felicidad</th>
+                    <th>Fecha de nacimiento</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {mascotas.map((tamagotchi) => (
+                    <tr key={tamagotchi._id}>
+                      <td>{tamagotchi.nombre}</td>
+                      <td>{tamagotchi.nivelHambre}</td>
+                      <td>{tamagotchi.nivelEnergia}</td>
+                      <td>{tamagotchi.nivelFelicidad}</td>
+                      <td>{tamagotchi.fechaNacimiento}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
 
-        if (selectedGame !== '') {
-            fetchGameResults();
-        }
-    }, [selectedGame]);
-
-    const handleGameChange = (event) => {
-        setSelectedGame(event.target.value);
-    };
-
-    return (
-        <div>
-            <h1>Resultados de Juegos</h1>
-
-            <label htmlFor="gameSelect">Selecciona el juego:</label>
-            <select id="gameSelect" value={selectedGame} onChange={handleGameChange}>
-                <option value="">-- Selecciona un juego --</option>
-                <option value="game1">Juego 1</option>
-                <option value="game2">Juego 2</option>
-                {/* Añade más opciones según tus juegos */}
-            </select>
-
-            {selectedGame !== '' && gameResults.length > 0 && (
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Jugador</th>
-                            <th>Fecha y Hora</th>
-                            <th>Resultado</th>
-                            {/* Agrega más encabezados de columna según tus datos */}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {gameResults.map((result) => (
-                            <tr key={result._id}>
-                                <td>{result.nombre}</td>
-                                <td>{result.fechaHora}</td>
-                                <td>{result.resultado}</td>
-                                {/* Agrega más celdas según tus datos */}
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            )}
+          {selectedGame === "rpsls" && (
+            <div>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Fecha</th>
+                    <th>Jugador</th>
+                    <th>Resultado</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {partidas.map((partida) => (
+                    <tr key={partida._id}>
+                      <td>{partida.fecha}</td>
+                      <td>{partida.jugador}</td>
+                      <td>{partida.resultado}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
         </div>
-    );
-};
-
+      </div>
+      <Link to="/">
+        <button className="mt-5 left-0 mb-4 ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Volver
+        </button>
+      </Link>
+    </div>
+  );
+}
 export default Records;
